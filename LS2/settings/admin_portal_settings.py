62052fa4dd75c763d9cdc3c13bd260c4146b1eea
jdkizer9 <jdkizer9@users.noreply.github.com>
2018-08-01 22:21:44 -0400

    Updated admin portal login url / redirect settings to take into account the admin portal root

--- LS2/settings/admin_portal_settings.py
+++ LS2/settings/admin_portal_settings.py
@@ -3,9 +3,11 @@ from django.conf import settings
 def get_additional_settings(environ):
     additional_settings = {}
 
-    additional_settings["ADMIN_PORTAL_ROOT"] = environ.get('LS2_ADMIN_PORTAL_ROOT', 'admin/')
-    additional_settings["LOGIN_REDIRECT_URL"] ='/admin/'
-    additional_settings["LOGIN_URL"] = '/admin/login/'
-    additional_settings["LOGOUT_REDIRECT_URL"] = '/admin/'
+    admin_portal_root = environ.get('LS2_ADMIN_PORTAL_ROOT', 'admin/')
+    additional_settings["ADMIN_PORTAL_ROOT"] = admin_portal_root
+
+    additional_settings["LOGIN_REDIRECT_URL"] = "/" + admin_portal_root
+    additional_settings["LOGIN_URL"] =  "/" + admin_portal_root + "login/"
+    additional_settings["LOGOUT_REDIRECT_URL"] =  "/" + admin_portal_root
     
     return additional_settings
