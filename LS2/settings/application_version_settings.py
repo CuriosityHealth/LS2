a2e2d6f765781b0890113b63611a09e90a1b79b4
James Kizer <james.kizer@gmail.com>
2019-07-03 13:53:48 -0400

    updated dependencies

--- LS2/settings/application_version_settings.py
+++ LS2/settings/application_version_settings.py
@@ -1,6 +1,6 @@
 def get_additional_settings(environ):
     additional_settings = {}
 
-    additional_settings['APPLICATION_VERSION'] = '1.10.7'
+    additional_settings['APPLICATION_VERSION'] = '1.10.8'
     
     return additional_settings
