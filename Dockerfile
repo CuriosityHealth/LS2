4be0f25d7cddae380d725d22f71b951f2c6debbf
James Kizer <james.kizer@gmail.com>
2019-12-09 07:37:06 -0700

    Changed docker base image to resolve issue and updated dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.6
+FROM python:3.6-stretch
 MAINTAINER James Kizer <james@curiosityhealth.com>
 
 ENV PYTHONUNBUFFERED 1
--- LS2/settings/application_version_settings.py
+++ LS2/settings/application_version_settings.py
@@ -1,6 +1,6 @@
 def get_additional_settings(environ):
     additional_settings = {}
 
-    additional_settings['APPLICATION_VERSION'] = '1.10.9'
+    additional_settings['APPLICATION_VERSION'] = '1.10.10'
     
     return additional_settings
